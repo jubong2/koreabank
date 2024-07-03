96d59d4733e512892bf8fbb98e2f915e398203d5
jubong <twoweekyear2@gmail.com>
2024-07-03 13:12:08 +0900

    완성

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+  $(document).ready(function(){
+    // tapmenu
+    $(function(){
+        $(".tabmenu > li >a").click(function(){
+            // console.log(this);
+            $(this).parent().addClass("active").siblings().removeClass("active")
+        })
+    })
+})
